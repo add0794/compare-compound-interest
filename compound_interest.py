5aa25cd2d663cd43230fc637aeeed905a554e381
add0794 <add0794@gmail.com>
2024-02-20 15:46:40 -0500

    add execution time counter

--- compound_interest.py
+++ compound_interest.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import time
 
 
 def interest(P: int, r: float, n: int, t: int) -> float:
@@ -9,16 +10,23 @@ def interest(P: int, r: float, n: int, t: int) -> float:
 
 # Get a numpy array using the interest function
 
-years = input("How many years do you plan on investing for? ")
+principal = int(input("How much money do you plan in investing? "))
+times_per_year = int(input("How often will the interest be compounded per year? "))
+years = int(input("How many years do you plan on investing for? "))
 low_interest_str = input("What's the lowest interest rate you're willing to take? ")
 high_interest_str = input("What's the *most reasonable* highest interest rate you're willing to take? ")
 
 # Convert input strings to floats and round to two decimal places
-low_interest = round(float(low_interest_str.rstrip('%')) / 100, 4)
-high_interest = round(float(high_interest_str.rstrip('%')) / 100, 4)
+low_interest = round(float(low_interest_str.rstrip('%')) / 100, 2)
+high_interest = round(float(high_interest_str.rstrip('%')) / 100, 2)
 
 # Prompt the user to input the number of samples they want to generate
 num_samples = int(input("How many samples do you want to generate? "))
+
+
+# Measure start time
+start_time = time.perf_counter()
+
 
 # Initiating the array
 all_values = []
@@ -28,14 +36,19 @@ all_values = []
 for j in np.linspace(low_interest, high_interest, num_samples):
     values_for_j = []  # Initialize a list to store the interest values for the current interest rate
     # Iterate over the years
-    for i in np.linspace(0, int(years) + 1, int(years)):
+    for i in np.linspace(0, years + 1, years):
         # Calculate the interest and append it to the list
-        values_for_j.append(interest(500, j, 4, i))
+        values_for_j.append(interest(principal, j, times_per_year, i))
     # Append the interest values for the current interest rate to the main list
     all_values.append(values_for_j)
 
 # Convert the list of lists into a NumPy array
 numpy_array = np.array(all_values)
+
+
+# Measure time it took to complete the program
+passed_time = time.perf_counter() - start_time
+print(f"It took {passed_time} seconds")
 
 
 # Plot the interest values using matplotlib
@@ -50,7 +63,7 @@ x = range(1, number_of_years + 1)
 # Plot each array separately
 for i in range(number_of_arrays):
     y = numpy_array[i, :]
-    plt.plot(x, y, label=f'Interest at {interest_rates[i]:3f}')  # Format interest rate to 3 decimal places
+    plt.plot(x, y, label=f'Interest at {interest_rates[i]:2f}')  # Format interest rate to 3 decimal places
 
 plt.xlabel('Year')
 plt.ylabel('Future Value')
