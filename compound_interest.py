8ca3e22c6a8fbecc13d8c97d974486030c6fc4e8
add0794 <add0794@gmail.com>
2024-02-21 19:13:55 -0500

    Add function for input prompts & debugging

--- compound_interest.py
+++ compound_interest.py
@@ -1,5 +1,7 @@
+import json
 import matplotlib.pyplot as plt
 import numpy as np
+import re
 import time
 
 
@@ -7,21 +9,49 @@ def interest(P: int, r: float, n: int, t: int) -> float:
     A = round( P * (1 + r/n)**(n*t), 2 )
     return A
 
-
 # Get a numpy array using the interest function
 
-principal = int(input("How much money do you plan in investing? ")) # The principal can only be an integer
-times_per_year = int(input("How often will the interest be compounded per year? "))
-years = int(input("How many years do you plan on investing for? "))
-low_interest_str = input("What's the lowest interest rate you're willing to take? ")
-high_interest_str = input("What's the *most reasonable* highest interest rate you're willing to take? ")
+with open("currency_symbols.json", "r") as json_file:
+    currency_symbols = json.load(json_file)
 
-# Convert input strings to floats and round to two decimal places
-low_interest = round(float(low_interest_str.rstrip('%')) / 100, 2)
-high_interest = round(float(high_interest_str.rstrip('%')) / 100, 2)
+def get_input(prompt, currency_symbol=None, input_type=None):
+    while True:
+        try:
+            user_input = input(prompt)
 
-# Prompt the user to input the number of samples they want to generate
-num_samples = int(input("How many samples do you want to generate? "))
+            if currency_symbol is not None:
+                if user_input not in currency_symbols.values():
+                    raise ValueError("You entered an incorrect value. Please enter a correct value.")
+                else:
+                    result = user_input
+
+            elif input_type == int and isinstance(user_input, str): 
+                user_input = re.sub(r'\D', '', user_input)
+                result = input_type(user_input)
+
+            elif input_type == float and isinstance(user_input, str):
+                user_input = re.sub(r'[^0-9.]', '', user_input)
+                result = round(input_type(user_input) / 100, 3)
+
+            else:
+                result = user_input
+            
+            return result
+
+        except Exception:
+            print("\nYou entered an incorrect value. Please enter a correct value.")
+
+
+# Get values for each variable in the interest function
+unit_of_currency = get_input("What unit of currency do you use? ", "$")
+principal = get_input("How much money do you plan in investing? ", None, int)
+times_per_year = get_input("How often will the interest be compounded per year? ", None, int)
+years = get_input("How many years do you plan on investing for? ", None, int)
+low_interest = get_input("What's the lowest interest rate you're willing to take? ", None, float)
+high_interest = get_input("What's the *most reasonable* highest interest rate you're willing to take? ", None, float)
+num_samples = get_input("How many samples do you want to generate? ", None, int)
+
+print([unit_of_currency, principal, times_per_year, years, low_interest, high_interest, num_samples])
 
 
 # Measure start time
@@ -41,6 +71,7 @@ for j in np.linspace(low_interest, high_interest, num_samples):
         values_for_j.append(interest(principal, j, times_per_year, i))
     # Append the interest values for the current interest rate to the main list
     all_values.append(values_for_j)
+
 
 # Convert the list of lists into a NumPy array
 numpy_array = np.array(all_values)
@@ -65,9 +96,15 @@ for i in range(number_of_arrays):
     y = numpy_array[i, :]
     plt.plot(x, y, label=f'Interest at {interest_rates[i]:2f}')  # Format interest rate to 3 decimal places
 
+# Set the limits of the plot
+plt.xlim(1, years)  # Set the x-axis limit to start at 0 and automatically adjust the maximum value
+plt.ylim(0, None)  # Set the y-axis limit to start at 0 and automatically adjust the maximum value
+
+# Add labels and title
 plt.xlabel('Year')
 plt.ylabel('Future Value')
 plt.title('Future Value over 25 Years for Different Interest Rates')
 plt.legend() # Add a legend to distinguish each array
 
+print(numpy_array)
 plt.show()
